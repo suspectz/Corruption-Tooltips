49c1cdd6be176010a3069713c3b1e84c24c845c4
behhNa <49240528+behhNa@users.noreply.github.com>
2020-02-18 19:15:12 +0100

    Create deDE.lua (#11)

---
+++ Locales/deDE.lua
@@ -0,0 +1,16 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CorruptionTooltips", "deDE", true)
+if not L then return end
+
+L["I"] = "I"
+L["II"] = "II"
+L["III"] = "III"
+
+-- Config
+L["Append to corruption stat"] = "Hinzufügen zum Verderbnis-Wert"
+L["Use the new style tooltip."] = "Benutze neuen Style für den Tooltip."
+L["Show summary on the corruption tooltip"] = "Zeige Zusammenfassung am Verderbnis-Tooltip"
+L["List your corruptions in the eye tooltip in the character screen."] = "Liste Verderbnis-Effekt im Auge-Tooltip in der Charakterinfo auf."
+L["Display in English"] = "Zeige alles in Englisch"
+L["Don't translate the corruption effect names."] = "Übersetze nicht die Verderbnis-Effekte."
+L["Show icon"] = "Zeige Icon"
+L["Show the spell icon along with the name."] = "Zeige das Icon am Verderbnis-Effekt an."
