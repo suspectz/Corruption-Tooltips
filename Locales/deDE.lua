cd33b8a0976c90de523edd70f7dd051fa3f951d9
Maurice Pape <mauricepape@users.noreply.github.com>
2020-02-25 19:22:56 +0100

    Update deDE.lua (#15)
    * Update deDE.lua
    
    Proposal for a better translation.
    
    It's generally not a good idea to do a 1:1 translation from English to German.
    
    * Update deDE.lua
    
    Fixed slight slip-up.

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -2,11 +2,11 @@ local L = LibStub("AceLocale-3.0"):NewLocale("CorruptionTooltips", "deDE")
 if not L then return end
 
 -- Config
-L["Append to corruption stat"] = "Hinzufügen zum Verderbnis-Wert"
-L["Use the new style tooltip."] = "Benutze neuen Style für den Tooltip."
-L["Show summary on the corruption tooltip"] = "Zeige Zusammenfassung am Verderbnis-Tooltip"
-L["List your corruptions in the eye tooltip in the character screen."] = "Liste Verderbnis-Effekt im Auge-Tooltip in der Charakterinfo auf."
-L["Display in English"] = "Zeige alles in Englisch"
-L["Don't translate the corruption effect names."] = "Übersetze nicht die Verderbnis-Effekte."
-L["Show icon"] = "Zeige Icon"
-L["Show the spell icon along with the name."] = "Zeige das Icon am Verderbnis-Effekt an."
+L["Append to corruption stat"] = "Am Verderbnis-Wert anhängen"
+L["Use the new style tooltip."] = "Neuen Tooltip-Stil verwenden."
+L["Show summary on the corruption tooltip"] = "Zusammenfassung am Verderbnis-Tooltip anzeigen"
+L["List your corruptions in the eye tooltip in the character screen."] = "Verderbnis-Effekte im Auge-Tooltip in der Charakterinfo auflisten."
+L["Display in English"] = "Nur Englisch verwenden"
+L["Don't translate the corruption effect names."] = "Verderbnis-Effekte nicht übersetzen."
+L["Show icon"] = "Icon anzeigen"
+L["Show the spell icon along with the name."] = "Verderbnis-Effekt-Icon samt Namen anzeigen."
