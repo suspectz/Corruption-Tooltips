db9a486e9ed7fdfc0f4f5b8445f60b41d4a572d7
LiangYuxuan <lyx@lyx.es>
2020-04-29 18:51:30 +0800

    Update zhCN (#22)

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -7,41 +7,41 @@ L["II"] = "2 级"
 L["III"] = "3 级"
 
 -- Config (Header)
-L["Version"] = "Version"
-L["Author"] = "Author"
+L["Version"] = "版本"
+L["Author"] = "作者"
 
 -- Config (General)
-L["General"] = "General"
-L["Per-character configuration"] = "Per-character configuration"
+L["General"] = "通用"
+L["Per-character configuration"] = "角色独立设置"
 L["Display in English"] = "显示英语腐化特效名称"
 L["Don't translate the corruption effect names."] = "不要使用本地化的腐化特效名称。"
 
 -- Config (Item Tooltips)
-L["Item Tooltips"] = "Item Tooltips"
+L["Item Tooltips"] = "物品鼠标提示"
 L["Append to corruption stat"] = "附加到腐蚀属性后"
 L["Use the new style tooltip."] = "使用新的鼠标提示外观，将腐蚀特效名称显示到腐蚀属性后。"
 L["Show icon"] = "显示图标"
 L["Show the spell icon along with the name."] = "在腐化特效名称前显示其图标。"
 
 -- Config (Summary)
-L["Summary"] = "Summary"
-L["Show summary on the corruption tooltip"] = "在鼠标提示中列出腐蚀特效"
-L["List your corruptions in the eye tooltip in the character screen."] = "在角色界面的腐化值鼠标提示中，列出你的腐蚀特效。"
-L["Show corruption amount in the character screen"] = "Show corruption amount in the character screen"
-L["Show corruption stat on items in the character screen when displaying the corruption tooltip."] = "Show corruption stat on items in the character screen when displaying the corruption tooltip."
+L["Summary"] = "总览"
+L["Show summary on the corruption tooltip"] = "在腐蚀总览鼠标提示中列出腐蚀特效"
+L["List your corruptions in the eye tooltip in the character screen."] = "在角色界面的腐蚀总览鼠标提示中，列出你的腐蚀特效。"
+L["Show corruption amount in the character screen"] = "在角色界面中显示腐蚀值"
+L["Show corruption stat on items in the character screen when displaying the corruption tooltip."] = "显示腐蚀总览鼠标提示时，在角色界面中显示对应物品的腐蚀值。"
 
 -- Config (Icons)
-L["Icons"] = "Icons"
-L["Show corruption icon atop of item in character screen and bags"] = "Show corruption icon atop of item in character screen and bags"
-L["Show N'Zoth label on all corrupted items"] = "Show N'Zoth label on all corrupted items"
-L["Icon border color"] = "Icon border color"
-L["Icon position"] = "Icon position"
-L["Bottom Left"] = "Bottom Left"
-L["Bottom Right"] = "Bottom Right"
-L["Top Left"] = "Top Left"
-L["Top Right"] = "Top Right"
+L["Icons"] = "图标"
+L["Show corruption icon atop of item in character screen and bags"] = "在物品图标上显示腐化特效图标"
+L["Show N'Zoth label on all corrupted items"] = "在所有腐化物品上显示恩佐斯标签"
+L["Icon border color"] = "图标边框颜色"
+L["Icon position"] = "图标位置"
+L["Bottom Left"] = "左下"
+L["Bottom Right"] = "右下"
+L["Top Left"] = "左上"
+L["Top Right"] = "右上"
 
 -- Reload
-L["Reload UI to apply changes?"] = "Reload UI to apply changes?"
-L["Reload"] = "Reload"
-L["Later"] = "Later"
+L["Reload UI to apply changes?"] = "重载界面以应用配置？"
+L["Reload"] = "重载"
+L["Later"] = "稍后"
