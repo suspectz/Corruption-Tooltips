54d656047053b32d462c937f3fbd5cb98e3d5754
Roman Butakov <1267001+SlipCore@users.noreply.github.com>
2020-05-01 13:28:20 +0300

    Update ruRU (#27)

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -7,41 +7,41 @@ L["II"] = "2 ур."
 L["III"] = "3 ур."
 
 -- Config (Header)
-L["Version"] = "Version"
-L["Author"] = "Author"
+L["Version"] = "Версия"
+L["Author"] = "Автор"
 
 -- Config (General)
-L["General"] = "General"
-L["Per-character configuration"] = "Per-character configuration"
+L["General"] = "Общее"
+L["Per-character configuration"] = "Индивидуальные настройки для персонажа"
 L["Display in English"] = "Отображать английские названия"
 L["Don't translate the corruption effect names."] = "Не переводить названия эффектов порчи."
 
 -- Config (Item Tooltips)
-L["Item Tooltips"] = "Item Tooltips"
-L["Append to corruption stat"] = "Добавить в характеристики порчи"
+L["Item Tooltips"] = "Подсказки предметов"
+L["Append to corruption stat"] = "Добавить название порчи в подсказки"
 L["Use the new style tooltip."] = "Используйте новый стиль подсказки."
-L["Show icon"] = "Показать значок"
+L["Show icon"] = "Показать значок порчи"
 L["Show the spell icon along with the name."] = "Показать значок заклинания вместе с названием."
 
 -- Config (Summary)
-L["Summary"] = "Summary"
+L["Summary"] = "Сводка"
 L["Show summary on the corruption tooltip"] = "Показать сводку во всплывающей подсказке"
 L["List your corruptions in the eye tooltip in the character screen."] = "Отображать список эффектов порчи во всплывающей подсказке на экране персонажа."
-L["Show corruption amount in the character screen"] = "Show corruption amount in the character screen"
-L["Show corruption stat on items in the character screen when displaying the corruption tooltip."] = "Show corruption stat on items in the character screen when displaying the corruption tooltip."
+L["Show corruption amount in the character screen"] = "Показывать количество порчи в окне персонажа"
+L["Show corruption stat on items in the character screen when displaying the corruption tooltip."] = "Показывать количество порчи на предметах экипировки в окне персонажа, когда отображается сводка."
 
 -- Config (Icons)
-L["Icons"] = "Icons"
-L["Show corruption icon atop of item in character screen and bags"] = "Показывать значок порчи поверх предмета в окне персонажа и сумках"
-L["Show N'Zoth label on all corrupted items"] = "Show N'Zoth label on all corrupted items"
-L["Icon border color"] = "Icon border color"
-L["Icon position"] = "Icon position"
-L["Bottom Left"] = "Bottom Left"
-L["Bottom Right"] = "Bottom Right"
-L["Top Left"] = "Top Left"
-L["Top Right"] = "Top Right"
+L["Icons"] = "Значки порчи"
+L["Show corruption icon atop of item in character screen and bags"] = "Показывать значки порчи поверх предметов в окне персонажа и сумках"
+L["Show N'Zoth label on all corrupted items"] = "Показывать метку Н'Зота на всех предметах с порчей"
+L["Icon border color"] = "Цвет границы значка"
+L["Icon position"] = "Положение значка"
+L["Bottom Left"] = "Внизу Слева"
+L["Bottom Right"] = "Внизу Справа"
+L["Top Left"] = "Вверху Слева"
+L["Top Right"] = "Вверху Справа"
 
 -- Reload
-L["Reload UI to apply changes?"] = "Reload UI to apply changes?"
-L["Reload"] = "Reload"
-L["Later"] = "Later"
+L["Reload UI to apply changes?"] = "Перезагрузить интерфейс для внесения изменений?"
+L["Reload"] = "Перезагрузить"
+L["Later"] = "Позже"
